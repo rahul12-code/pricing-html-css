e1581ba879994799bace8aa2265c5fc5217dc183
rahul <rahulkandati@gmail.com>
2024-11-02 10:50:44 +0530

    removed hover effect and added color to box2

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 
 
-// Select the toggle switch 
+// Select the toggle switch
 const toggleSwitch = document.getElementById("toggleSwitch");
-
-toggleSwitch.addEventListener("keydown", (event) => {
-  if (event.key === "Enter" || event.key === " ") {
-    event.preventDefault();
-    toggleSwitch.checked = !toggleSwitch.checked;  // Toggle the state
-    updatePrices();  
-  }
-}); 
 
 toggleSwitch.addEventListener("change", updatePrices);
 
 function updatePrices() {
+  // Select the price elements 
   const price1 = document.getElementById("price1");
   const price2 = document.getElementById("price2");
   const price3 = document.getElementById("price3");
 
+  
   price1.textContent = toggleSwitch.checked ? "19.99" : "199.99";
   price2.textContent = toggleSwitch.checked ? "24.99" : "249.99";
   price3.textContent = toggleSwitch.checked ? "39.99" : "399.99";
 }
+
 
 
